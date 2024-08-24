42accc9b84401ae9cf5990f58a621e58009d861c
Jvxi <2806949963@qq.com>
2024-08-24 21:20:12 +0800

    Update README.md
    改动

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class main {
     //一个main方法
     public static void main(String[] args) {
         System.out.println("Hello JAVA!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
